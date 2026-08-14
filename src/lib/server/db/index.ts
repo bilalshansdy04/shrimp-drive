ceeb3e099be6c56d45ec32d7df09c3e2cc337580
bilalshansdy04 <bilalshandy04@gmail.com>
2026-08-14 19:41:40 +0700

    feat: migrate database configuration and client implementation to Turso

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -3,8 +3,12 @@ import { createClient } from '@libsql/client';
 import * as schema from './schema';
 import { env } from '$env/dynamic/private';
 
-if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+if (!env.TURSO_DATABASE_URL) throw new Error('TURSO_DATABASE_URL is not set');
+if (!env.TURSO_AUTH_TOKEN) throw new Error('TURSO_AUTH_TOKEN is not set');
 
-const client = createClient({ url: env.DATABASE_URL });
+const client = createClient({ 
+	url: env.TURSO_DATABASE_URL,
+	authToken: env.TURSO_AUTH_TOKEN
+});
 
 export const db = drizzle(client, { schema });
