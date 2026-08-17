0da578d5fb673a82b34b639d7b7c92d9d825bc25
bilalshansdy04 <bilalshandy04@gmail.com>
2026-08-17 18:52:44 +0700

    fix: alias libsql/client to web to force drizzle to use web version

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,11 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+	resolve: {
+		alias: {
+			'@libsql/client': '@libsql/client/web'
+		}
+	},
 	plugins: [
 		tailwindcss(),
 		sveltekit({
